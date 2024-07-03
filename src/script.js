5aad14cbc49278631ca8d55e8c2ae80e9da594dc
Giada Sponzilli <sponzilli.g@gmail.com>
2024-07-03 11:39:21 +0100

    resize images and pseudo code for next steps

--- src/script.js
+++ src/script.js
@@ -8,9 +8,24 @@ document.addEventListener("DOMContentLoaded", ()=> {
       img.src = article.image
       img.alt = article.title;
       img.dataset.id = article.id;
-      img.classList.add('w-full', 'h-auto', 'rounded', 'shadow-md');
+      img.style.width = 'auto';
+      img.style.height = '100px';
+      img.style.margin = '100px 10px 50px 10px';
+      img.classList.add('rounded', 'shadow-md');
+     // img.classList.add('w-auto', 'h-24', 'm-4', 'rounded', 'shadow-md');
       imgList.appendChild(img)
     });
   })
   .catch(error => console.error('Error fetching articles:', error));
 })
+
+
+// the function should say healine displaying ??? now should be replaced with the real headline which was hide.
+
+// the button reveal headline should wait for an event listener
+
+// when the page is opened the first card created is the last json file
+// we need a function that dynamically creates a card with all the info of the article.
+// add event listner of images, when images are clicked the previous function executes and the previous card disappear.
+
+// the images should be displayed in reverse json file order
