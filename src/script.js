915fcc3f1d305cca55174a86838d74a7142b0c71
Giada Sponzilli <sponzilli.g@gmail.com>
2024-07-04 16:16:33 +0100

    added code for generate first article of the json list when page is opened

--- src/script.js
+++ src/script.js
@@ -2,8 +2,38 @@ document.addEventListener("DOMContentLoaded", ()=> {
   fetch("./articles.json")
   .then(response => response.json())
   .then(data => {
-    const imgList = document.getElementById("imagesContainer")
-    data.forEach(article => {
+
+    const imgList = document.getElementById("imagesContainer");
+
+    //Generate first CardGuessTheHeadline when page is open
+
+    const lastArticle = data[data.length - 1];
+    //Image Card
+    const imgCardDiv = document.getElementById("imgCardContainer");
+    const imgCard = document.createElement("img");
+    imgCard.src = lastArticle.image;
+    imgCard.alt = lastArticle.altTextImg;
+    imgCard.classList.add(   
+      'rounded-lg');
+    imgCardDiv.appendChild(imgCard);
+
+    //Text Card
+    const textContainer = document.getElementById("textContainer");
+    const pTextCard = document.createElement("p");
+    pTextCard.textContent = lastArticle.content;
+    pTextCard.classList.add('p-8');
+    textContainer.appendChild(pTextCard);
+    const pAuthorTextCard = document.createElement("p");
+    pAuthorTextCard.textContent = lastArticle.author
+    pAuthorTextCard.classList.add('font-bold');
+    pAuthorTextCard.style.paddingTop = '2rem';
+    pTextCard.appendChild(pAuthorTextCard)
+
+
+
+
+    // Iterate through each element in the json file and print a small image on the page as a menu
+    data.reverse().forEach(article => {
       const img = document.createElement("img");
       img.src = article.image
       img.alt = article.title;
@@ -18,6 +48,10 @@ document.addEventListener("DOMContentLoaded", ()=> {
   })
   .catch(error => console.error('Error fetching articles:', error));
 })
+
+
+
+
 
 
 // The function should say headline displaying ??? now should be replaced with the real headline which was hide.
@@ -52,8 +86,10 @@ function revealHeadline() {
 revealHeadlineButton.addEventListener("click", revealHeadline)
 
 
-// when the page is opened the first card created is the last json file
-// we need a function that dynamically creates a card with all the info of the article.
+//// when the page is opened the first card created is the last json file
+// we need a function that dynamically updates the card info with all the info of the article.
 // add event listner of images, when images are clicked the previous function executes and the previous card disappear.
+// Hover on navBar button and reveal headline button
 
-// the images should be displayed in reverse json file order
+// add arrows to go to next article
+////the images should be displayed in reverse json file order
