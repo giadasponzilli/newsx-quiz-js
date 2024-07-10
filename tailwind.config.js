cd9289cfc24c459b5ec5017a166af72ff1deed7e
Giada Sponzilli <sponzilli.g@gmail.com>
2024-07-10 15:52:07 +0100

    styled the form

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       'custom-color-purple': '#B4B1D4', // Define your custom color here
       'custom-color-grey': '#585858',
       'custom-color-purple-hover': '#645BC1',
+      'custom-color-purple-gradient': '#923B99',
     },
     fontFamily: {
       serif: ['Times New Roman', 'Times', 'serif'],
