0f31649905d7d22462e8cf90b7b74496b5e43e5b
Giada Sponzilli <sponzilli.g@gmail.com>
2024-08-17 08:27:44 +0200

    redeploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html",
-  "./public/**/*.js", "./public/styles/**/*.css"],
+  content: ["/public/**/*.html",
+  "/public/**/*.js", "/public/styles/**/*.css"],
   theme: {
     extend: {
       colors: {
