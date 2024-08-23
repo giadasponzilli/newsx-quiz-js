30ce6d644366ffc1270420843d6bde4dce83f0d6
Giada Sponzilli <sponzilli.g@gmail.com>
2024-08-23 15:50:52 +0100

    adjusted relative path for json

--- src/js/script.js
+++ src/js/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Variable to store the articles data
   let articles = [];
 
-  fetch("../../public/data/articles.json")
+  fetch("./data/articles.json")
     .then(response => response.json())
     .then(data => {
       articles = data;
